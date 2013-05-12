9cb18c8ed905070a22948096e5bc67d1f4ba119f
Bill Gathen <bill@billgathen.com>
2013-05-11 21:30:23 -0400

    Tweaked JS per JShint

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,19 @@
+/* global _ */
 'use strict';
 
 angular.module('mainApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.fizzbuzz = function() {
-      if ( invalidNum($scope.lastNum) ) return [];
+      if ( invalidNum($scope.lastNum) ) { return []; }
       return _.map(_.range(1,$scope.lastNum+1), function(num) {
-        if (num % 15 === 0) return "FizzBuzz";
-        if (num % 3 === 0) return "Fizz";
-        if (num % 5 === 0) return "Buzz";
+        if (num % 15 === 0) { return 'FizzBuzz'; }
+        if (num % 3 === 0) { return 'Fizz'; }
+        if (num % 5 === 0) { return 'Buzz'; }
         return num;
       });
     };
 
     var invalidNum = function(num) {
-      return (typeof num === 'undefined') || num > 100 || num < 0
+      return (typeof num === 'undefined') || num > 100 || num < 0;
     };
   }]);
